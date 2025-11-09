a881080e9c248bfb7d308529942f39696c57339c
NicoNico222 <wlsdnbilly@naver.com>
2025-11-09 20:24:00 +0900

    초기 커밋.

---
+++ LIG_CapstoneDlg.cpp
@@ -0,0 +1,147 @@
+﻿
+// LIG_CapstoneDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "LIG_Capstone.h"
+#include "LIG_CapstoneDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+
+
+
+CLIGCapstoneDlg::CLIGCapstoneDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_LIG_CAPSTONE_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CLIGCapstoneDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CLIGCapstoneDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_CTLCOLOR() // 배경색
+END_MESSAGE_MAP()
+
+
+
+BOOL CLIGCapstoneDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	SetIcon(m_hIcon, TRUE);		
+	SetIcon(m_hIcon, FALSE);	
+
+	m_brushBg.CreateSolidBrush(RGB(64, 64, 64));
+
+	return TRUE; 
+}
+
+void CLIGCapstoneDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+void CLIGCapstoneDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this);
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+HCURSOR CLIGCapstoneDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+HBRUSH CLIGCapstoneDlg::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor) // 배경 색 변경
+{
+	HBRUSH hbr = CDialogEx::OnCtlColor(pDC, pWnd, nCtlColor);
+
+	if (nCtlColor == CTLCOLOR_DLG)
+	{
+		return m_brushBg;
+	}
+
+	return hbr;
+}
---
+++ LIG_CapstoneDlg.h
@@ -0,0 +1,32 @@
+﻿
+// LIG_CapstoneDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+class CLIGCapstoneDlg : public CDialogEx
+{
+public:
+	CLIGCapstoneDlg(CWnd* pParent = nullptr);
+
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LIG_CAPSTONE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+	CBrush m_brushBg; // 배경화면 색
+
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+	DECLARE_MESSAGE_MAP()
+};
